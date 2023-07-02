476a8e9b69ff32c08c1a0826a219b43c17f2922e
geeknonerd <geek@best2geek.com>
2023-07-02 13:28:36 +0800

    modify Args support more

--- proxy/schema.py
+++ proxy/schema.py
@@ -1,6 +1,6 @@
 from typing import List, Literal
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, validator
 
 import g4f
 
@@ -14,6 +14,12 @@ class Args(BaseModel):
     messages: List[Message]
     model: str
     stream: bool = False
+    temperature: float = Field(1.0, ge=0, le=2.0)
+    top_p: float = Field(1.0, ge=0, le=1.0)
+    n: int = Field(1, ge=1)
+    max_tokens: int = Field(None, le=4096)
+    presence_penalty: float = Field(0, ge=-2.0, le=2.0)
+    frequency_penalty: float = Field(0, ge=-2.0, le=2.0)
 
     @validator('model')
     def validate_model(cls, m):
