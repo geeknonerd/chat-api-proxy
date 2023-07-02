8daf1d7b5442909078a009a8d3c41dc1c47c2a85
geeknonerd <geek@best2geek.com>
2023-07-02 08:20:30 +0800

    fix Bearer Authentication

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -6,11 +6,11 @@ async def auth_by_token(request: Request):
     """Auth by token, if token is None, then skip"""
     from .config import get_settings
 
-    token = request.headers.get('token', '')
+    token = request.headers.get('Authorization', '')
     auth_token = get_settings().auth_token
     if not auth_token:
         return
-    if token.lstrip('Bearer ') == auth_token:
+    if token == f'Bearer {auth_token}':
         return
     raise HTTPException(status_code=403)
 
